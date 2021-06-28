756247a2aebff6a68fdc7737c0f4a50cba2b0d7f
mersonfufu@gmail.com <mersonfufu@gmail.com>
2021-06-28 16:16:19 -0300

    Big update CVH

---
+++ Utilidades/JitMacro-FbUnsend.mcr
@@ -0,0 +1,5 @@
+Mouse : 0 : 0 : Click : 0 : 1 : 0
+DELAY : 500
+Mouse : C-25 : C-45 : Click : 0 : 0 : 0
+DELAY : 500
+Mouse : 970 : 545 : Click : 0 : 0 : 0
---
+++ Utilidades/NotepadPP-Tema.bat
@@ -0,0 +1,13 @@
+@echo off
+rem /**
+rem  * mersonfufu@gmail.com
+rem  * ultima update em 11:01 09/04/2020
+rem  */
+
+:INICIO
+set Folder="%AppData%\Notepad++\Themes\"
+
+rem COMANDOS
+copy /y "NotepadPP-Tema-Visual Studio 2012 Dark.xml" %Folder%\"Visual Studio 2012 Dark.xml"
+
+:FIM
