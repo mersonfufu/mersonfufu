2a23bf5b3d5806e8aeb2b76219894a9a9330b447
mersonfufu@gmail.com <mersonfufu@gmail.com>
2025-05-11 13:10:04 -0300

    Flexible fix

---
+++ Utilidades/Configuracoes.bat
@@ -0,0 +1,15 @@
+:: /**
+::  * mersonfufu@gmail.com
+::  * ultima update em 15:13 24/11/2022
+::  */
+
+@echo off
+
+:: APARENCIA
+color 0A
+mode 60,15
+
+:INICIO
+copy /y "Flexible Renamer.ini" "%UserProfile%\Documents\Flexible Renamer\"
+
+:FIM
